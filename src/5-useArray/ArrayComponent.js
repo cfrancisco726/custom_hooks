f0bc6d73dc68caf2af0d858ac0af17fa0d7c1072
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-01-27 18:43:31 -0500

    useFetch hook

--- src/5-useArray/ArrayComponent.js
+++ src/5-useArray/ArrayComponent.js
@@ -1,23 +1,21 @@
-import { useArray } from "./useArray";
+import useArray from "./useArray";
 
 export default function ArrayComponent() {
   const { array, set, push, remove, filter, update, clear } = useArray([
     1, 2, 3, 4, 5, 6,
   ]);
+
   return (
     <div>
-      <div>{array.join(",")}</div>
-      <button onClick={() => push(7)}>add 7</button>
-      <button onClick={() => update(1, 9)}>Change Second element to 9</button>
+      <div>{array.join(", ")}</div>
+      <button onClick={() => push(7)}>Add 7</button>
+      <button onClick={() => update(1, 9)}>Change Second Element To 9</button>
       <button onClick={() => remove(1)}>Remove Second Element</button>
       <button onClick={() => filter((n) => n < 3)}>
-        Keep numbers less than 4
+        Keep Numbers Less Than 4
       </button>
-      <button onClick={() => filter((n) => n < 3)}>
-        keep numbers less than 4
-      </button>
-      <button onCLick={() => set([1, 2])}>Set to 1, 2</button>
-      <button onCLick={clear}>clear</button>
+      <button onClick={() => set([1, 2])}>Set To 1, 2</button>
+      <button onClick={clear}>Clear</button>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import TimeOutComponent from "./2-useTimeout/TimeoutComponent";
 import LocalStorageComponent from "./0-useLocalStorage/LocalStorageComponent";
 import DebounceComponent from "./3-useDebounce/DebounceComponent";
 import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
-// import UpdateEffectComponent from "./4-useUpdateEffect/TimeoutComponent/";
-// import ArrayComponent from "./5-useArray/ArrayComponent/";
+import ArrayComponent from "./5-useArray/ArrayComponent";
+import PreviousComponent from "./6-usePrevious/PreviousComponent";
+import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent";
+import StorageComponent from "./8-useStorage/StorageComponent";
+import AsyncComponent from "./9-useAsync/AsyncComponent";
+import FetchComponent from "./10-useFetch/FetchComponent";
 
 function App() {
   return (
@@ -12,9 +16,14 @@ function App() {
       {/* <LocalStorageComponent /> */}
       {/* <ToggleComponent /> */}
       {/* <TimeOutComponent /> */}
-      <DebounceComponent />
+      {/* <DebounceComponent /> */}
       {/* <UpdateEffectComponent /> */}
       {/* <ArrayComponent /> */}
+      {/* <PreviousComponent /> */}
+      {/* <StateWithHistoryComponent /> */}
+      {/* <StorageComponent /> */}
+      <AsyncComponent />
+      {/* <FetchComponent /> */}
     </div>
   );
 }
